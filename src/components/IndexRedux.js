483b618304159a7cdb2a1cd1db945463b2e3ad40
EdwinMLara <emlara35@gmail.com>
2020-07-10 13:32:28 -0500

    Implementado diferentes acciones en reducer

--- src/components/IndexRedux.js
+++ src/components/IndexRedux.js
@@ -3,6 +3,7 @@ const redux = require('../../node_modules/redux')
 const createStore = redux.createStore
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 function buyCake(){
     return {
@@ -11,16 +12,27 @@ function buyCake(){
     }
 }
 
-const incialState = {
-    numOfcakes: 10
+function buyIceCream(){
+    return {
+        type: BUY_ICECREAM
+    }
 }
+
+const incialState = {
+    numOfcakes: 10,
+    numOfIceCream: 20
+}
+
+
 
 const reducer = (state = incialState, action) =>{
     switch(action.type){
         case BUY_CAKE: return{
             ...state, numOfcakes: state.numOfcakes - 1
         }
-
+        case BUY_ICECREAM: return{
+            ...state, numOfIceCream: state.numOfIceCream - 1
+        }
         default: return state
     }
 }
@@ -30,5 +42,7 @@ console.log('Inicial state',store.getState())
 const unsubscribe = store.subscribe(() => console.log("Update state",store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
-store.dispatch(buyCake())   
+store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe()
