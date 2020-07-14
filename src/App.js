f7956597fab533a9e97a924e9928c3d26cf088b3
EdwinMLara <emlara35@gmail.com>
2020-07-14 15:14:19 -0500

    haciendo mas pruebas de redux

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import {Provider} from 'react-redux'
 import store from './redux/store'
 import HookCakeContainer from './components2/HookCakeContainer';
 import IceCreamContainer from './components2/IceCreamContainer';
+import NewCakeContainer from './components2/NewCakeContainer';
+import ItemContainer from './components2/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake/>
+        <ItemContainer/>
         <HookCakeContainer/>
         <CakeContainer/>
         <IceCreamContainer/>
+        <NewCakeContainer/>
       </div>
     </Provider>
   );
